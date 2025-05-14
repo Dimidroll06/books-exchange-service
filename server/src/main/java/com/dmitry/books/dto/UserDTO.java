70d84d0cc8dd1371a7f545a584698d6b8668e2b6
Dimidroll06 <dima.shved.06@gmail.com>
2025-05-14 15:57:31 +0300

    Добавлены swagger описания и json аннотации для UserDTO

--- server/src/main/java/com/dmitry/books/dto/UserDTO.java
+++ server/src/main/java/com/dmitry/books/dto/UserDTO.java
@@ -1,5 +1,8 @@
 package com.dmitry.books.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,13 +11,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDTO {
+    
+    @Schema(description = "Ник пользователя")
+    @JsonProperty("username")
     private String username;
+    
+    @Schema(description = "Идентификатор пользователя")
+    @JsonProperty("id")
     private Long id;
+    
+    @Schema(description = "Является ли пользователь администратором")
+    @JsonProperty("isAdmin")
     private boolean isAdmin;
 
-    public UserDTO(String username, long id, boolean isAdmin) {
-        this.username = username;
-        this.id = id;
-        this.isAdmin = isAdmin;
-    }
 }
